cc59e41bfc2f8ae982d25c444f952c1b25d8c581
Carl Yu <yuaooauy@yeah.net>
2016-11-21 17:21:06 +0800

    First

---
+++ FITDemo/Assets/ButtonClick.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ButtonClick : MonoBehaviour {
+
+    
+    private int id;
+    public void Click()
+    {
+        Debug.Log(this.name);
+        switch (modelStatus.status)
+        {
+            case Status.merged:
+                if (this.name == "split")
+                {
+                    GameObject.Find("root").GetComponent<ModelControl>().split();
+                    modelStatus.status = Status.split;
+                }
+                else
+                {
+                    Debug.Log("Merged now. Can't be done.");
+                }
+                break;
+            case Status.split:
+                if (this.name == "merge")
+                {
+                    GameObject.Find("root").GetComponent<ModelControl>().merge();
+                    modelStatus.status = Status.merged;
+                }
+                else if (this.name == "modelUp")
+                {
+
+                }
+                else if (this.name == "modelDown")
+                {
+
+                }
+                else if (this.name == "split")
+                {
+                    Debug.Log("Already split. Can't be done.");
+                }
+                else
+                {
+                    id = Convert.ToInt32(this.name);
+                    GameObject.Find("root").GetComponent<ModelControl>().choose(id);
+                    modelStatus.status = Status.single;
+                }
+                break;
+            case Status.single:
+                if (this.name == "1" || this.name == "2" || this.name == "3" || this.name == "4" || this.name == "5" || this.name == "6")
+                {
+                    GameObject.Find("root").GetComponent<ModelControl>().showAll();
+                    modelStatus.status = Status.split;
+                }
+                break;
+        }
+    }
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        modelStatus.status = Status.merged;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ FITDemo/Assets/ModelControl.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModelControl : MonoBehaviour {
+
+    private void init()
+    {
+        movingFlag = new bool[6];
+        for (int i = 0; i < 6; ++i)
+            movingFlag[i] = false;
+        startPoint = new Vector3[6];
+        targetPoint = new Vector3[6];
+    }
+	// Use this for initialization
+	void Start () {
+        init();
+	}
+
+    public Vector3 offset;
+    private bool[] movingFlag = { false, false, false, false, false, false };
+    private const int movingFrames = 50;
+    private int tick;
+    private Vector3[] startPoint;
+    private Vector3[] zeroPoint = { new Vector3(0, 0, 0), new Vector3(-24.07f, 3f, -3.46f), new Vector3(-30.78f, 6f, -12.01f), new Vector3(-49.33f, 9f, -16.5f), new Vector3(-49.34f, 12f, -16f), new Vector3(-49.86f, 15f, -16.2f) };
+    private Vector3[] splitPoint = { new Vector3(0, 0, 0), new Vector3(-24.07f, 3f+5, -3.46f), new Vector3(-30.78f, 6f+5*2, -12.01f), new Vector3(-49.33f, 9f+5*3, -16.5f), new Vector3(-49.34f, 12f+5*4, -16f), new Vector3(-49.86f, 15f+5*5, -16.2f) };
+    private Vector3[] targetPoint;
+	// Update is called once per frame
+	void FixedUpdate () {
+        tick++;
+        if (tick == movingFrames)
+        {
+            for (int i = 0; i < 6; ++i)
+            {
+                if (movingFlag[i])
+                {
+                    GameObject.Find("root").transform.Find((i + 1).ToString()).position = targetPoint[i];
+                    movingFlag[i] = false;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 6; ++i)
+            {
+                if (movingFlag[i])
+                {
+                    GameObject.Find("root").transform.Find((i + 1).ToString()).position += (targetPoint[i] - startPoint[i]) / movingFrames;
+                }
+            }
+        }
+	}
+    public void split()
+    {
+        for (int i = 0; i < 6; ++i)
+        {
+            movingFlag[i] = true;
+            tick = 0;
+            startPoint[i] = GameObject.Find("root").transform.Find((i + 1).ToString()).position;
+        }
+        targetPoint = splitPoint;
+    }
+    public void merge()
+    {
+        for (int i = 0; i < 6; ++i)
+        {
+            movingFlag[i] = true;
+            tick = 0;
+            startPoint[i] = GameObject.Find("root").transform.Find((i + 1).ToString()).position;
+        }
+        targetPoint = zeroPoint;
+    }
+    public void choose(int a)
+    {
+        for (int i = 0; i < 6; ++i)
+        {
+            if (i+1 != a)
+            {
+                GameObject.Find("root").transform.Find((i + 1).ToString()).gameObject.SetActive(false);
+            }
+        }
+    }
+    public void showAll()
+    {
+        for (int i = 0; i < 6; ++i)
+        {
+            GameObject.Find("root").transform.Find((i + 1).ToString()).gameObject.SetActive(true);
+        }
+    }
+}
